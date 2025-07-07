97d07f1213900e94117a1301d166d354eb43a124
thalles-cg <thalles.pity@gmail.com>
2025-07-07 17:06:08 -0300

    fix: Estilos inline sobrescrevendo o CSS da preview

--- utils.js
+++ utils.js
@@ -5,13 +5,45 @@ export function redirecionarParaPreview(path = '') {
     window.location.href = `./preview.html${path ? '#' + path : ''}`;
   }
 }
-
 export function applyInlineStyles(element) {
   const computedStyle = window.getComputedStyle(element);
+
+  const excludedProps = [
+    'width',
+    'height',
+    'min-width',
+    'max-width',
+    'min-height',
+    'max-height',
+    'flex-basis', 
+    'flex-grow',
+    'flex-shrink',
+    'grid-template-columns', 
+    'grid-template-rows',
+    'margin', 
+    'margin-top',
+    'margin-right',
+    'margin-bottom',
+    'margin-left',
+    'padding', 
+    'padding-top',
+    'padding-right',
+    'padding-bottom',
+    'padding-left',
+    'top', 'bottom', 'left', 'right', 
+    'position', 
+    'float', 
+    'clear',
+    'display', 
+    'vertical-align', 
+  ];
+
   for (const prop of computedStyle) {
-    try {
-      element.style[prop] = computedStyle.getPropertyValue(prop);
-    } catch (e) {
+    if (!excludedProps.includes(prop)) {
+      try {
+        element.style[prop] = computedStyle.getPropertyValue(prop);
+      } catch (e) {
+      }
     }
   }
 
